f14c9769dec357ec7579b0d8b9ef43d53baad277
hhwf <329589951@qq.com>
2019-11-25 11:00:54 +0800

    init

---
+++ schedule-admin/src/test/java/com/hhwf/scheduleadmin/ScheduleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhwf.scheduleadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
